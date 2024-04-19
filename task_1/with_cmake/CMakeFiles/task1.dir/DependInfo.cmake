3724a248cd22faa018c80752e063bc6c1d7b697d
vladospuchkov <v.puchkov@g.nsu.ru>
2024-04-19 17:26:47 +0700

    migration from repositories

---
+++ task_1/with_cmake/CMakeFiles/task1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vladislave/coding/paralelka/task_1_sinus/with_cmake/lab1.cpp" "CMakeFiles/task1.dir/lab1.cpp.o" "gcc" "CMakeFiles/task1.dir/lab1.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
